cdb0c4566de16b05583bd6f8c5b5d9183cdf9614
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-01-20 21:08:01 -0500

    Variables_Constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,19 @@
+var nombre ="Jasbeith";
+let edad = 16;
+
+var profesor = false;
+var Lista_cursos = ["Javascript, photoshop"];
+
+var informacion_personal = {
+    id: 123456,
+    direccion: "calle 22",
+};
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(Lista_cursos);
+console.log(informacion_personal);
+
+const cedula= 12345;
+console.warn(cedula);
